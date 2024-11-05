1f063171ed78f7e89a3095419d5dcd2bbd5c1d22
sharkbird <jason@sharkbird.com>
2024-11-04 20:29:01 -0600

    fixed camera hanging.  cleaned up.

--- fire.mjs
+++ fire.mjs
@@ -1,26 +1,20 @@
 #!/usr/bin/env zx
 
-// first arg is number of photos, second is wait time between photos.
+// first arg is number of photos, second is wait time between photos in seconds.
+//import { $ } from 'zx'
 
 const args=process.argv.slice(2)
 const numberofpics = args[1];
 const waittime=args[2]*1000;
-let again
-var result = `ls`
-var reset = 'gphoto2 --reset'
-var shoot = 'gphoto2 -- '
-let orderName
-while ( orderName = await question('eneter order name: ')) {
+let orderName = ''
 
+while ( orderName = await question('enter order name: ')) {
   await $`mkdir ${orderName}`
   cd( `${orderName}`)
-
   for (let i=0;i<numberofpics;i++) {
-    const output1 = (await $`ls -al`).stdout;
-    console.log(output1)
+    await $`gphoto2 --capture-image-and-download --filename "${orderName}-${i}.jpg"`
     await sleep(waittime)
   }
   cd('..')
- const output=(await $`pwd`).stdout;
-  console.log(`${output}`);
+  await $`gphoto2 --reset`
 }
